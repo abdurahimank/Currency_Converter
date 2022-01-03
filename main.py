b4bd1e745d7d8d92f16d1d15a9cf7d64323072ca
Abdurahiman K <laluvga@gmail.com>
2022-01-03 07:35:21 +0530

    just starting

---
+++ main.py
@@ -0,0 +1,3 @@
+print("Welcome to the Currency Converter")
+currency_from = input("Please select the currency which you want to convert")
+currency_to = input("Please select the currency to which you want to convert")
