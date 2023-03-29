0486a8603ba942c589f4d87bfd7fa7ba88cfc27c
ABDURAHIMAN K <laluvga@gmail.com>
2023-03-29 10:35:53 +0530

    Stage 1/6: Cryptocurrencies are the new black

--- stages.py
+++ stages.py
@@ -1,7 +1,2 @@
-# Stage 4/6: Going global
-no_coincoin = float(input())
-print(f"""I will get {round(no_coincoin * 2.98, 2)} RUB from the sale of {round(no_coincoin, 1)} conicoins.
-I will get {round(no_coincoin * 0.82, 2)} ARS from the sale of {round(no_coincoin, 1)} conicoins.
-I will get {round(no_coincoin * 0.17, 2)} HNL from the sale of {round(no_coincoin, 1)} conicoins.
-I will get {round(no_coincoin * 1.9622, 2)} AUD from the sale of {round(no_coincoin, 1)} conicoins.
-I will get {round(no_coincoin * 0.208, 2)} MAD from the sale of {round(no_coincoin, 1)} conicoins.""")
+# Stage 1/6: Cryptocurrencies are the new black
+print("Meet a conicoin!")
