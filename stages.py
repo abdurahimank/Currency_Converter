4cd7cb9944bc473423c0820fa1816382b91b83c9
ABDURAHIMAN K <laluvga@gmail.com>
2023-03-29 10:41:51 +0530

    Stage 2/6: Talking numbers

--- stages.py
+++ stages.py
@@ -1,2 +1,5 @@
-# Stage 1/6: Cryptocurrencies are the new black
-print("Meet a conicoin!")
+# Stage 2/6: Talking numbers
+no_coincoin = int(input(""))
+print(f"""I have {no_coincoin} conicoins.
+{no_coincoin} conicoins cost {no_coincoin * 100} dollars.
+I am rich! Yippee!""")
