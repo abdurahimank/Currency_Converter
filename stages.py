342bf96e557d4b4ab47a1c42c7ba6f69d683b6cc
ABDURAHIMAN K <laluvga@gmail.com>
2023-03-29 10:44:53 +0530

    Stage 4/6: Going global

--- stages.py
+++ stages.py
@@ -1,4 +1,7 @@
-# Stage 3/6: More interaction
-no_coincoin = int(input("Please, enter the number of conicoins you have: "))
-exchange_rate = float(input("Please, enter the exchange rate: "))
-print(f"The total amount of dollars: {round(no_coincoin * exchange_rate, 2)}")
+# Stage 4/6: Going global
+no_coincoin = float(input())
+print(f"""I will get {round(no_coincoin * 2.98, 2)} RUB from the sale of {round(no_coincoin, 1)} conicoins.
+I will get {round(no_coincoin * 0.82, 2)} ARS from the sale of {round(no_coincoin, 1)} conicoins.
+I will get {round(no_coincoin * 0.17, 2)} HNL from the sale of {round(no_coincoin, 1)} conicoins.
+I will get {round(no_coincoin * 1.9622, 2)} AUD from the sale of {round(no_coincoin, 1)} conicoins.
+I will get {round(no_coincoin * 0.208, 2)} MAD from the sale of {round(no_coincoin, 1)} conicoins.""")
