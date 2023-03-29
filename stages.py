c55fb68e3662d70b76b95aa03be35244964ca4ca
ABDURAHIMAN K <laluvga@gmail.com>
2023-03-29 10:33:23 +0530

    Delete stages.py

--- stages.py
+++
@@ -1,4 +0,0 @@
-# Stage 4/6: Going global
-no_coincoin = int(input("Please, enter the number of conicoins you have: "))
-exchange_rate = float(input("Please, enter the exchange rate: "))
-print(f"The total amount of dollars: {round(no_coincoin * exchange_rate, 2)}")
