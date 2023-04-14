0067f19655169a933a52a032e7d9860f86a7f08e
ABDURAHIMAN K <laluvga@gmail.com>
2023-04-14 08:50:36 +0530

    Stage 5/6: JSON and the Rates

--- stages.py
+++ stages.py
@@ -1,7 +1,10 @@
-# Stage 4/6: Going global
-no_coincoin = float(input())
-print(f"""I will get {round(no_coincoin * 2.98, 2)} RUB from the sale of {round(no_coincoin, 1)} conicoins.
-I will get {round(no_coincoin * 0.82, 2)} ARS from the sale of {round(no_coincoin, 1)} conicoins.
-I will get {round(no_coincoin * 0.17, 2)} HNL from the sale of {round(no_coincoin, 1)} conicoins.
-I will get {round(no_coincoin * 1.9622, 2)} AUD from the sale of {round(no_coincoin, 1)} conicoins.
-I will get {round(no_coincoin * 0.208, 2)} MAD from the sale of {round(no_coincoin, 1)} conicoins.""")
+# Stage 5/6: JSON and the Rates
+import requests
+import json
+
+
+currency = input().lower()
+r = requests.get(f"http://www.floatrates.com/daily/{currency}.json")
+dic = json.loads(r.text)
+print(dic["usd"])
+print(dic["eur"])
